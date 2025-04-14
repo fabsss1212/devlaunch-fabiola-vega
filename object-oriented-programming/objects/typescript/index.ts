7dd739319494f810cb6bd21cbcf62ca38f9a3364
fabsss1212 <164280877+fabsss1212@users.noreply.github.com>
2025-04-14 02:17:45 +0000

    chore(objects): add ts changed

--- object-oriented-programming/objects/typescript/index.ts
+++ object-oriented-programming/objects/typescript/index.ts
@@ -6,3 +6,15 @@ const message: string ='hello world'
 var something: string 
 
 console.log('hi')
+
+
+function greet(name, callback) {
+  console.log("Hi " + name);
+  callback();
+}
+
+function sayBye() {
+  console.log("Bye!");
+}
+
+greet("Alice", sayBye);
